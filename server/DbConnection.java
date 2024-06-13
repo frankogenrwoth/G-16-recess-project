e2eb1b09d5a1e8f8dc814bbb824f7e84616682a9
Ogenrwoth frank <121643701+frankogenrwoth@users.noreply.github.com>
2024-06-13 21:14:53 +0300

    define the structure and definitions for creating connection between the database and the server

--- server/DbConnection.java
+++ server/DbConnection.java
@@ -1,4 +1,36 @@
 package server;
 
+import org.json.JSONObject;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DbConnection {
+    // initialize all these attributes
+    String url;
+    String username;
+    String password;
+    Connection connection;
+    Statement statement = connection.createStatement();
+
+    public DbConnection() throws SQLException {
+        // initialize a connection here and save the connection
+    }
+
+    public void create(String sqlCommand) throws SQLException {
+        this.statement.execute(sqlCommand);
+    }
+
+    public void read(String sqlCommand) throws SQLException {
+        this.statement.execute(sqlCommand);
+    }
+
+    public void update(String sqlCommand) throws SQLException {
+        this.statement.execute(sqlCommand);
+    }
+
+    public void delete(String sqlCommand) throws SQLException {
+        this.statement.execute(sqlCommand);
+    }
 }
